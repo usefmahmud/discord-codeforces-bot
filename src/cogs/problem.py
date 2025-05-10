d78bc529d424d954ee46ffa85f1a0a346ab0c9cb
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-05-10 12:37:30 +0300

    feat(problem): enhance /problem command to accept optional tag filter for random problem retrieval

--- src/cogs/problem.py
+++ src/cogs/problem.py
@@ -2,15 +2,17 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from src.models.problem import ProblemManager
+from src.models.problem import LITERAL_TAGS
+from typing import Optional
 
 class Problem(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name="problem", description="Get a random problem")
-    async def problem(self, interaction: discord.Interaction):
-
-        problem = ProblemManager.get_random_problem()
+    async def problem(self, interaction: discord.Interaction, tag: Optional[LITERAL_TAGS] = None):
+        print('tag', tag)
+        problem = ProblemManager.get_random_problem(tag)
         if problem:
             print('problem', problem)
             await interaction.response.send_message(f"Problem: {problem['name']}\nRating: {problem['rating']}\nTags: {', '.join(problem['tags'])}")
--- src/models/problem.py
+++ src/models/problem.py
@@ -14,8 +14,24 @@ class ProblemManager():
                 cursor.execute('''
                     SELECT * FROM problems
                     JOIN problem_tags ON problems.problem_id = problem_tags.problem_id
-                    WHERE problem_tags.tag_id = ?
+                    JOIN tags ON problem_tags.tag_id = tags.tag_id
+                    WHERE tags.tag_name = ?
+                    ORDER BY RANDOM()
+                    LIMIT 1
                 ''', (tag,))
+                problem = cursor.fetchone()
+
+                problem_tags = cursor.execute('SELECT tag_name FROM problem_tags JOIN tags ON problem_tags.tag_id = tags.tag_id WHERE problem_id = ?', (problem[0],)).fetchall()
+
+                if problem and problem_tags:
+                    return {
+                        'problem_id': problem[0],
+                        'contest_id': problem[1],
+                        'index': problem[2],
+                        'name': problem[3],
+                        'rating': problem[4],
+                        'tags': [tag[0] for tag in problem_tags],
+                    }
             else:
                 problems_count = cursor.execute('SELECT COUNT(*) FROM problems').fetchone()[0]
                 random_problem_id = random.randint(1, problems_count)
